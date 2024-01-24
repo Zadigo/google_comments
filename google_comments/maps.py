1fc5863a316edbf36dc8866f2e93bdb8d50cc41f
Zadigo <pendenquejohn@gmail.com>
2024-01-24 11:36:39 +0100

    Code refactoring

--- google_comments/maps.py
+++ google_comments/maps.py
@@ -58,8 +58,7 @@ class SpiderMixin:
         self.feed_scroll_counter = Counter()
 
         if output_folder is not None:
-            self.output_folder_path = self.output_folder_path.joinpath(
-                output_folder)
+            self.output_folder_path = self.output_folder_path.joinpath(output_folder)
             if not self.output_folder_path.exists():
                 self.output_folder_path.mkdir()
 
