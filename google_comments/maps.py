383f5d7b454cd1f6bcd37af3899979ebf9e6bdb7
Zadigo <pendenquejohn@gmail.com>
2024-02-14 11:40:15 +0100

    Change parameter name

--- google_comments/maps.py
+++ google_comments/maps.py
@@ -498,7 +498,7 @@ class GooglePlace(GoogleMapsMixin):
             self.driver.maximize_window()
 
         # if not self.keep_unique_file and self.filename is not None:
-        self.filename = filename = create_filename(suffix=id_or_reference)
+        self.filename = filename = create_filename(suffix=self.scrap_session_id)
         self.driver.get(url)
 
         # 1. Click on the consent form - This appears
